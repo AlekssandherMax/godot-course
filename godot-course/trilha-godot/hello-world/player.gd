fe2e6416bcaa8198b1e2c5ed5060e10f50686097
AlekssandherMax <alekssandher1@hotmail.com>
2024-05-02 20:06:32 -0300

    atualizações nas aulas godot

--- godot-course/trilha-godot/hello-world/player.gd
+++ godot-course/trilha-godot/hello-world/player.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@export var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	position += input * speed * delta
+	
 	pass
