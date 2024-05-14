352b14392f0b0a900ff740fa3f96d446fc284fbb
AlekssandherMax <alekssandher1@hotmail.com>
2024-05-14 19:19:45 -0300

    Atualização no industrial

--- godot-course/trilha-godot/plataformIndustrial/script/player.gd
+++ godot-course/trilha-godot/plataformIndustrial/script/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-
-const SPEED = 450.0
+@export var speed = 3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _physics_process(delta):
 	# Add the gravity.
 	var direction = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
-	velocity = direction * SPEED
-
+	#velocity = direction * speed * 150
+	
+	var targetVelocity = direction * speed * 150
+	velocity = lerp(velocity, targetVelocity, 0.5)
 	move_and_slide()
