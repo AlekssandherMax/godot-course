ef1f52020d21f34802da5fc88b6d965f735819b2
AlekssandherMax <alekssandher1@hotmail.com>
2024-06-16 19:05:45 -0300

    godot

--- godot-course/trilha-godot/plataformIndustrial/script/player.gd
+++ godot-course/trilha-godot/plataformIndustrial/script/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-@export var speed = 3
+@export var speed = 4
+@export_range(0, 1) var lerpVector = 0.1
+@onready var sprite = %sprite
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _physics_process(delta):
@@ -9,5 +11,7 @@ func _physics_process(delta):
 	#velocity = direction * speed * 150
 	
 	var targetVelocity = direction * speed * 150
-	velocity = lerp(velocity, targetVelocity, 0.5)
+	velocity = lerp(velocity, targetVelocity, lerpVector)
 	move_and_slide()
+	var target_rotation = direction.x * 25
+	sprite.rotation_degrees = lerp(sprite.rotation_degrees, target_rotation, lerpVector)
